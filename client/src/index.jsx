03608bc3a49a9d4697f7c77f3a3a3591e7655090
Davyd Zakorchennyi <Diza41a@mail.com>
2022-05-21 14:24:58 -0400

    StarRating component added (with neccessary styling files)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,17 @@
-// Bring React in to build a component.
+/* eslint-disable import/extensions */
 import React from 'react';
-// Import from react-dom the ability to create a root render
 import { createRoot } from 'react-dom/client';
-// create the root of the app by selection where the app should be mounted in the dom
+
+// Major component imports
+import ItemOverview from './ItemOverview/ItemOverview.jsx';
+
 const root = createRoot(document.getElementById('root'));
 
-// creates component in js syntax (hence, no <> allowed, js doesn't know what it means)
-// const App = React.createElement("h1",null,"Hello World");
-// root.render(<App />); -> will NOT render!! root.render(App); -> will
-
-class App extends React.Component {
-  render () {
-    return <h1> Hello World</h1>
-  }
-};
+function App() {
+  return (
+    <ItemOverview />
+  );
+}
 
 // render the root element with the provided component
 root.render(<App />);
